9f8ac91ecdc9bce91ffd19ec6cff7f141437a1a4
Tuominiemi William <william.tuominiemi@aalto.ficd>
2025-01-05 15:56:13 +0200

    initialize function

--- game.cpp
+++ game.cpp
@@ -1,11 +1,29 @@
 #include "SDL2/SDL.h"
 #include <iostream>
 
+using namespace std;
+
+bool init()
+{
+    bool success = true;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        success = false;
+    }
+
+    return success;
+}
+
 int main(int argc, char *args[])
 {
-    SDL_Init(SDL_INIT_EVERYTHING);
-
-    SDL_Quit();
-
+    if (!init())
+    {
+        cout << "Failed to initialize. " << endl;
+    }
+    else
+    {
+        cout << "Initialized. " << endl;
+    }
     return 0;
 }
